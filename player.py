790bfa72e2b67b32ce42dbcab9a69e0e1b756555
trev <szenty@gmail.com>
2014-12-06 10:09:48 +0100

    simple logic

--- player.py
+++ player.py
@@ -1,9 +1,22 @@
+def have_high_cards(my_cards):
+    return "A" in my_cards or "K" in my_cards
+
 
 class Player:
     VERSION = "ALL IN + some hole card stuff"
 
     def betRequest(self, game_state):
-        return 1000
+        offer = 100
+        my_cards = get_hole_card_ranks(game_state)
+
+        if have_high_cards(my_cards):
+            offer = 1000
+        card_values = {"A": 100,
+                       "K": 50,
+                       "Q": 40}
+
+
+        return offer
 
     def showdown(self, game_state):
         pass
