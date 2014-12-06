424ff0d471484409fbffb35c9fbd47d8ce08720d
akoskaaa <akos.hochrein@prezi.com>
2014-12-06 10:29:38 +0100

    master test

---
+++ tests/test_player.py
@@ -0,0 +1,28 @@
+
+from unittest import TestCase
+from player import Player
+
+
+class TestPlayer(TestCase):
+
+    def setUp(self):
+        self.player = Player()
+        self.mock_game_state_ok = {
+            'in_action': 0,
+            'players': [
+                {
+                    'hole_cards': [
+                        {
+                            'rank': "A"
+                        },
+                        {
+                            'rank': "K"
+                        }
+                    ]
+                }
+            ]
+        }
+
+    def test_player_returns_int(self):
+        hopefully_int = self.player.betRequest(self.mock_game_state_ok)
+        assert type(hopefully_int) == int
