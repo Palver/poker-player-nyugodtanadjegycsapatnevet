2784eb265a051c562666255f40a481d6bb56488b
akoskaaa <akos.hochrein@prezi.com>
2014-12-06 09:59:58 +0100

    hole card getting

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "ALL IN + some hole card stuff"
 
     def betRequest(self, game_state):
         return 1000
@@ -8,3 +8,8 @@ class Player:
     def showdown(self, game_state):
         pass
 
+
+def get_hole_card_ranks(game_state):
+    player_id = game_state['in_action']
+    cards = game_state['players'][player_id]['hole_cards']
+    return [card['rank'] for card in cards]
