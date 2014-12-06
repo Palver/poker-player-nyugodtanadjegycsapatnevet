e656689711f4811bad954fe714240a19a13748bd
trev <szenty@gmail.com>
2014-12-06 09:46:24 +0100

    commit test

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+import random
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+
         return 0
 
     def showdown(self, game_state):
