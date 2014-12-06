fd24bf042fdcc5b2ebb5b27876e0cbd6a8c0b3c8
akoskaaa <akos.hochrein@prezi.com>
2014-12-06 11:12:44 +0100

    card count map

--- card_ranking.py
+++ card_ranking.py
@@ -1,4 +1,6 @@
 
+from collections import defaultdict
+
 
 class RankAgent(object):
 
@@ -20,3 +22,19 @@ class RankAgent(object):
 
     def is_pair(self):
         return False
+
+    def is_drill(self):
+        
+        for key in card_count_map.keys():
+            if card_count_map[key] == 3:
+                return True
+
+        return False
+
+    def _get_card_count_map(self):
+        card_count_map = dict()
+        for card in self.cards:
+            card_count_map[card['rank']] = card_count_map.get(card_rank['rank'], 0) + 1
+
+        return card_count_map
+
