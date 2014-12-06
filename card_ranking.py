3b40e7e53272cc18975d4f235ea77825cc58eb0e
trev <szenty@gmail.com>
2014-12-06 11:50:31 +0100

    ranking update: flush

--- card_ranking.py
+++ card_ranking.py
@@ -4,14 +4,15 @@ from collections import defaultdict
 
 class RankAgent(object):
 
-
     def __init__(self, cards):
         self.cards = cards
-        self._card_count = dict()
+        self._card_rank_count = dict()
+        self._card_suit_count = dict()
         self._count_cards()
 
     def get_card_rank(self):
         card_rank = 0
+
 
         if self._is_pair():
             card_rank = 1
@@ -19,6 +20,8 @@ class RankAgent(object):
             card_rank = 2
         if self._is_drill():
             card_rank = 3
+        if self.__is_flush():
+            card_rank = 4
 
         card_rank += 1
         return card_rank
@@ -27,18 +30,27 @@ class RankAgent(object):
         return False
 
     def _is_pair(self):
-        return 2 in self._card_count.values()
+        return 2 in self._card_rank_count.values()
 
     def _is_drill(self):
-        return 3 in self._card_count.values()
+        return 3 in self._card_rank_count.values()
 
     def _is_two_pairs(self):
-        count_list = list(self._card_count.values())
+        count_list = list(self._card_rank_count.values())
         number_of_pairs = count_list.count(2)
         return 2 == number_of_pairs
+
+    def _is_flush(self):
+        return max(self._card_suit_count.values()) >= 5
+
+
 
     def _count_cards(self):
         for card in self.cards:
             rank = card['rank']
-            self._card_count[rank] = self._card_count.get(rank, 0) + 1
+            suit = card['suit']
+            self._card_rank_count[rank] = self._card_rank_count.get(rank, 0) + 1
+            self._card_suit_count[suit] = self._card_suit_count.get(suit, 0) + 1
+
+
 
